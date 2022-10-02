51461447e97a07450a4c57be78e0051e4e80da83
Nelson Alexandre Mutane <inkomomutane@github.com>
2022-10-02 12:03:43 +0200

    Folder link made and app service provider string lenght is up to 191 from 125

--- config/filesystems.php
+++ config/filesystems.php
@@ -70,7 +70,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+        base_path(env('PUBLIC_FOLDER_PATH')) => storage_path('app/public'),
     ],
 
 ];
