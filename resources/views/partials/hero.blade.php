cf87f2169d1bc9666dfa968fbc0e57e3a06e4f90
jfm-dev <justivenciof@gmail.com>
2024-07-17 15:17:56 +0200

    Adicao de imagem grupo na pagina inicial

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -1,38 +1,3 @@
-<!-- <section class="bg-gradient-to-b from-white  via-amber-50 to-zinc-100 dark:bg-gray-900">
-    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:px-32  lg:grid-cols-12">
-   
-
-        <div class="mr-auto place-self-center lg:col-span-6 tracking-tight p-8">
-
-            <h2 class="max-w-2xl mb-4 text-2xl font-extrabold tracking-tight leading-none md:text-3xl xl:text-4xl
-             dark:text-white first-letter:uppercase" data-aos="fade-down">Sumburero</h2>
-            <h1 class="max-w-2xl mb-4 text-gray-600 text-sm font-extrabold tracking-tight leading-none
-             dark:text-white first-letter:uppercase " data-aos="fade-down">— Grupo técnico de proteção social </h1>
-
-            <p class="max-w-2xl  mb-6 font-light text-gray-700 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400" data-aos="fade-down">
-                Promovendo o desenvolvimento sustentável nas comunidades, garantindo o acesso aos direitos fundamentais da criança...</p>
-            <a href="{{ route('about.us') }}" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-sm font-medium text-center text-gray-900
-             bg-primary-100 hover:bg-primary-200 focus:ring-4 focus:ring-primary-300
-              " data-aos="fade-down">
-                Ler mais
-                <svg class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                </svg>
-            </a>
-        </div>
-        <div class="hidden lg:mt-0 lg:col-span-6 lg:flex">
-            <div class="grid  grid-cols-3 gap-2">
-                <img class="col-span-2 row-span-2  h-full object-cover object-top rounded" src="{{ Vite::asset('resources/images/hero.webp')}}" alt="mockup" data-aos="fade-left">
-                <img class="h-full object-cover object-left rounded " src="{{ Vite::asset('resources/images/hero.webp')}}" alt="mockup" data-aos="fade-left">
-
-                <img class="h-full object-cover object-left rounded  border-primary-100" src="{{ Vite::asset('resources/images/hero.webp')}}" alt="mockup" data-aos="fade-left">
-
-            </div>
-
-        </div>
-    </div>
-</section> -->
-
 <section class="relative bg-gradient-to-b to-amber-100 from-white dark:bg-gray-900">
     <img class="absolute inset-0 w-full h-full object-cover" src="{{ Vite::asset('resources/images/bg-3.webp') }}" alt="mockup">
     <div class="absolute   inset-0 bg-black opacity-20"></div>
